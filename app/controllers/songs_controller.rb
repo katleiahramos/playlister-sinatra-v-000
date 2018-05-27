10e93657d68f617ce589d397f6951c10e402795a
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-27 00:30:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,14 +12,21 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-  @song = Song.create(:name => params["Name"])
-  @song.artist = Artist.find_or_create_by(:name => params["ArtistName"])
-  @song.genre_ids = params[:genres]
-  @song.save
+    @song = Song.create(params[:song])
+    params[:genres].each do |genre|
+      @song.genres << Genre.find_by(genre[:id])
+    end
 
-  flash[:message] = "Successfully created song."
+    if !params["artist"]["name"].empty? && !Artist.find_by(:name => params["artist"]["name"])
+      @song.artist = Artist.create(name: params["artist"]["name"])
+    elsif Artist.find_by(:name => params["artist"]["name"])
+      @song.artist = Artist.find_by(:name => params["artist"]["name"])
+    end
 
-  redirect("/songs/#{@song.slug}")
+    @song.save
+
+    flash[:message] = "Successfully created song."
+    redirect to("/songs/#{@song.slug}")
   end
 
   get '/songs/:slug/edit' do
@@ -28,15 +35,22 @@ class SongsController < ApplicationController
   end
 
   post '/songs/:slug' do
-  @song = Song.find_by_slug(params[:slug])
-  @song.update(params[:song])
-  @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
-  @song.genre_ids = params[:genres]
-  @song.save
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params["song"])
 
-   flash[:message] = "Successfully updated song."
-   redirect("/songs/#{@song.slug}")
- end
+
+    if !params["artist"]["name"].empty? && !Artist.find_by(:name => params["artist"]["name"])
+      @song.artist = Artist.create(name: params["artist"]["name"])
+    elsif Artist.find_by(:name => params["artist"]["name"])
+      @song.artist = Artist.find_by(:name => params["artist"]["name"])
+    end
+
+    @song.save
+
+    flash[:message] = "Successfully updated song."
+    redirect to("/songs/#{@song.slug}")
+
+  end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
